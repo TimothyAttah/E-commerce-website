40ac2335ba2e7dba57cfd7d121b117c09e27dd68
Timothy Attah <timothyattah733@gmail.com>
2021-03-24 23:14:49 +0100

    Added create router with express and mongoose

--- controllers/expensesController.js
+++ controllers/expensesController.js
@@ -6,8 +6,21 @@ const getExpenses = async ( req, res ) => {
     const expenses = await Expenses.find();
     res.status( 201 ).json( { message: 'Success', expenses } );
   } catch (error) {
-    res.status(500).json({error: error.message})
+    res.status( 500 ).json( { error: error.message } );
   }
 };
 
+const createExpenses = async ( req, res ) => {
+  const expenses = req.body;
+  try {
+    if ( !expenses.content || !expenses.values ) return res.status( 404 ).json( { error: 'Please fill in all fields' } );
+    const newExpenses = await new Expenses( expenses );
+    await newExpenses.save();
+    res.status( 201 ).json( { message: 'Expenses was created successfully', newExpenses } );
+  } catch (error) {
+    res.status( 500 ).json( { error: error.message } );
+  }
+}
+
 module.exports.getExpenses = getExpenses;
+module.exports.createExpenses = createExpenses;
--- routes/expensesRoutes.js
+++ routes/expensesRoutes.js
@@ -1,8 +1,10 @@
 const express = require( 'express' );
 const router = express.Router();
 
-const { getExpenses } = require( '../controllers/expensesController' );
+const { getExpenses, createExpenses } = require( '../controllers/expensesController' );
 
 router.get( '/', getExpenses );
+
+router.post( '/create', createExpenses );
 
 module.exports = router;
