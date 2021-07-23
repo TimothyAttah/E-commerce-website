c185bc863bd4019742883491fcf331104221683f
Timothy Attah <timothyattah733@gmail.com>
2021-07-23 13:47:45 +0100

    Created update user route

--- controllers/usersControllers.js
+++ controllers/usersControllers.js
@@ -1,12 +1,38 @@
+const bcrypt = require( 'bcryptjs' );
+const User = require( '../models/UserModel' );
+
 const userControllers = {
   // UPDATE USER
+  updateUser: async ( req, res ) => {
+    const { password, userId } = req.body;
+    
+      if ( userId === req.params.id || req.user.isAdmin ) {
+        if ( password ) {
+          try {
+
+            password = await bcrypt.hash(password, 12)
+          } catch ( err ) {
+            return res.status(500).json({error: err})
+          }
+        }
+        try {
+          const user = await User.findByIdAndUpdate( req.params.id, {
+            $set: req.body
+          } )
+          res.status(200).json({message: 'Account has been updated.'})
+        } catch (err) {
+          return res.status(500).json({ error: err });
+        }
+      
+      } else {
+        return res.status(403).json({ error: 'You can update only your account!' });
+      }
+   
+  },
   // DELETE USER 
   // GET A USER
   // FOLLOW A USER
   // UNFOLLOW A USER
-  getUsers: async ( req, res ) => {
-    res.send('Hello welcome to users page')
-  }
 };
 
 module.exports = userControllers;
--- routes/usersRoute.js
+++ routes/usersRoute.js
@@ -1,7 +1,7 @@
 const router = require( 'express' ).Router();
 const userControllers = require( '../controllers/usersControllers' );
 
-router.get('/', userControllers.getUsers)
+router.put( '/update/:id', userControllers.updateUser );
 
 
 module.exports = router;
