1dc0992329ec2f3f86ff55788961a38c922df346
Timothy Attah <timothyattah733@gmail.com>
2021-07-23 14:00:30 +0100

    Created delete user route

--- controllers/usersControllers.js
+++ controllers/usersControllers.js
@@ -1,38 +1,54 @@
-const bcrypt = require( 'bcryptjs' );
-const User = require( '../models/UserModel' );
+const bcrypt = require('bcryptjs');
+const User = require('../models/UserModel');
 
 const userControllers = {
-  // UPDATE USER
-  updateUser: async ( req, res ) => {
-    const { password, userId } = req.body;
-    
-      if ( userId === req.params.id || req.user.isAdmin ) {
-        if ( password ) {
-          try {
+	// UPDATE USER
+	updateUser: async (req, res) => {
+		const { password, userId } = req.body;
 
-            password = await bcrypt.hash(password, 12)
-          } catch ( err ) {
-            return res.status(500).json({error: err})
-          }
-        }
-        try {
-          const user = await User.findByIdAndUpdate( req.params.id, {
-            $set: req.body
-          } )
-          res.status(200).json({message: 'Account has been updated.'})
-        } catch (err) {
-          return res.status(500).json({ error: err });
-        }
-      
-      } else {
-        return res.status(403).json({ error: 'You can update only your account!' });
-      }
-   
-  },
-  // DELETE USER 
-  // GET A USER
-  // FOLLOW A USER
-  // UNFOLLOW A USER
+		if (userId === req.params.id || req.body.isAdmin) {
+			if (password) {
+				try {
+					password = await bcrypt.hash(password, 12);
+				} catch (err) {
+					return res.status(500).json({ error: err });
+				}
+			}
+			try {
+				const user = await User.findByIdAndUpdate(req.params.id, {
+					$set: req.body,
+				});
+				res.status(200).json({ message: 'Account has been updated.' });
+			} catch (err) {
+				return res.status(500).json({ error: err });
+			}
+		} else {
+			return res
+				.status(403)
+				.json({ error: 'You can update only your account!' });
+		}
+	},
+
+	// DELETE USER
+	deleteUser: async (req, res) => {
+		const {userId, isAdmin } = req.body;
+
+		if (userId === req.params.id || isAdmin) {
+			try {
+				const user = await User.findByIdAndDelete(req.params.id);
+				res.status(200).json({ message: 'Account has been deleted.' });
+			} catch (err) {
+				return res.status(500).json({ error: err });
+			}
+		} else {
+			return res
+				.status(403)
+				.json({ error: 'You can delete only your account!' });
+		}
+	},
+	// GET A USER
+	// FOLLOW A USER
+	// UNFOLLOW A USER
 };
 
 module.exports = userControllers;
--- routes/usersRoute.js
+++ routes/usersRoute.js
@@ -3,5 +3,7 @@ const userControllers = require( '../controllers/usersControllers' );
 
 router.put( '/update/:id', userControllers.updateUser );
 
+router.delete( '/delete/:id', userControllers.deleteUser );
+
 
 module.exports = router;
