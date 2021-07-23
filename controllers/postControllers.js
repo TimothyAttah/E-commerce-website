f6c534218577f7538b9cf38b03455ccff30c4dc6
Timothy Attah <timothyattah733@gmail.com>
2021-07-23 15:29:17 +0100

    Added create post route

--- controllers/postControllers.js
+++ controllers/postControllers.js
@@ -2,6 +2,21 @@ const Post = require( '../models/PostModel' );
 
 const postControllers = {
 
+  // CREATE A POST
+  createPost: async ( req, res ) => {
+    const newPost = new Post( req.body );
+    try {
+      const savedPost = await newPost.save();
+      res.status( 201 ).json( { message: 'New post created successfully.', savedPost } );
+    } catch (err) {
+      return res.status(500).json({error: err})
+    }
+  }
+  // UPDATE A POST
+  // DELETE A POST
+  // LIKE A POST
+  // GET A POST
+  // GET TIMELINE POSTS
 }
 
 module.exports = postControllers;
--- routes/postRoute.js
+++ routes/postRoute.js
@@ -1,4 +1,6 @@
 const router = require( 'express' ).Router();
 const postControllers = require( '../controllers/postControllers' );
 
+router.post( '/create', postControllers.createPost );
+
 module.exports = router;
